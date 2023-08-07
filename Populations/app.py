753761f24a93999b2640c2721b0492c84780c29c
JP-Butler <justinpbutler12@gmail.com>
2023-08-07 01:31:23 -0400

    adding flask with all routes, readme from main

--- Populations/app.py
+++ Populations/app.py
@@ -1,12 +1,8 @@
 # Import dependencies 
 import pandas as pd
-from flask import Flask #, jsonify
+from flask import Flask 
 from flask_cors import CORS, cross_origin
-from sqlalchemy import create_engine #, func
-# import numpy as np
-# import sqlalchemy
-# from sqlalchemy.ext.automap import automap_base
-# from sqlalchemy.orm import Session
+from sqlalchemy import create_engine 
 
 
 #################################################
